1fc0a1f9ee16f1514d71b8fbf27b110f4c134dba
1jkl3 <3521941319@qq.com>
2019-12-14 14:55:57 +0800

    修复布局

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,54 +8,93 @@ Page({
     option: ["个性推荐", "歌单", "主播电台", "排行榜"],
     img: ["/assets/img/1.jpg", "/assets/img/2.jpg", "/assets/img/3.jpg", "/assets/img/4.jpg", "/assets/img/5.jpg"],
     current:0,
-    clientHeight:0,
-    istabFixed:true
+    istabFixed:false,
+    swiper_height:0,
+    scrollTop:0,
+    //异步测试
+    testdata:[]
   },
-  //切换主题
+  //点击切换主题
   tabbarindex(e){
-    this.setData({
+    var that = this
+    that.setData({
       current: e.detail.index
     })
   },
+  //滑动切换主题
   handletopic(e){
-    this.setData({
-      current: e.detail.current
+    var that = this
+    var topicindex = e.detail.current
+    that.setData({
+      current: topicindex
     })
+    //获取当前子组件dom
+    that.get_wxml(`.topic-view-${that.data.current}`, (res) => {
+      // console.log(res+"1")
+      that.setData({
+        swiper_height: res.height
+      });
+    })
+  },
+  //数据加载时动画
+  dataload(){
+    wx.lod
+  },
+  //监听页面滚动
+  handlescroll(e){
+    // console.log(e.detail.scrollTop)
+    if (e.detail.scrollTop >= this.data.scrollTop){
+      this.setData({
+        istabFixed:true
+      })
+    }else {
+      this.setData({
+        istabFixed: false
+      })
+    }
+  },
+  //封装获取组件、标签的属性方法
+  get_wxml(className,callbe) {
+    wx.createSelectorQuery().select(className).boundingClientRect(callbe).exec();
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    //异步测试
+    var arr = [1, 2, 3, 4, 56, 6, 7, 8, 9, 12, 10, 13, 14, 15, 16]
+    setTimeout(()=>{
+      this.setData({
+        testdata: arr
+      })
+    },5000)
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    //获取窗口大小
+    //首次加载时获取swiper子组件高度
     var that = this
-    wx.getSystemInfo({
-      success: function (res) {
-        console.log(res)
-        that.setData({
-          clientHeight: res.windowHeight
-        });
-      }
-    });
-    var reslut = wx.createSelectorQuery()
-    reslut.select("#tab").boundingClientRect()
-    reslut.selectViewport().scrollOffset()
-    reslut.exec(function(res){
-      console.log(res[0].top)
+    that.get_wxml(`.topic-view-${that.data.current}`,(res)=>{
+      // console.log(res)
+      that.setData({
+        swiper_height: res.height
+      });
     })
   },
-
+  
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    var that = this
+    that.get_wxml(`#tabout`, (res) => {
+      // console.log(res.top)
+      that.setData({
+        scrollTop:res.top
+      })
+    })
   },
 
   /**
